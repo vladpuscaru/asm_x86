7c5ec630e02c12bcb6110c4c294cd9af099bb769
Vlad Puscaru <vlad.cpuscaru@gmail.com>
2021-11-17 08:33:26 +0000

    2. Display string subroutine

---
+++ 2.display_string/dstring.asm
@@ -0,0 +1,40 @@
+section .data
+    text db "Hello, World!", 10, 0
+    text2 db "World?", 10, 0
+
+section .text
+    global _start
+
+_start:
+
+    mov rax, text
+    call _print
+
+    mov rax, text2
+    call _print
+
+    mov rax, 60
+    mov rax, 0
+    syscall
+
+
+;Print string subroutine
+;input: rax
+;output: print string at rax
+_print:
+    push rax
+
+    mov rbx, 0
+    _loop:
+        inc rax
+        inc rbx
+        mov cl, [rax]
+        cmp cl, 0
+        jne _loop
+
+    mov rax, 1
+    mov rdi, 1
+    pop rsi ;the input from rax
+    mov rdx, rbx
+    syscall
+    ret
